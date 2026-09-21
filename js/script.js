0b93f695d522ed5dadfa1926df56d7f471650262
dlwndmsindiyo-juju <dlwndmsindiyo@gmail.com>
2026-09-21 23:03:17 +0900

    헤더 메뉴 js 변수 선언

---
+++ js/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () =>{
+    gsap.registerPlugin(ScrollTrigger);
+
+    // 헤더 메뉴 드롭다운
+    const header = document.querySelector("header");
+    const subBg = document.querySelector(".sub-bg");
+    const mainMenus = document.querySelectorAll(".main-menu>li");
+
+    // 서브메뉴 배경 높이
+    const subMenuHeight = 200;
+    
+    
+})
