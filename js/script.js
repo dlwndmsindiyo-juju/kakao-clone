25a69b68f6481b53af0af4f4758696499005bdae
dlwndmsindiyo-juju <dlwndmsindiyo@gmail.com>
2026-09-22 19:59:48 +0900

    헤더 서브메뉴 슬라이드 스크립트 적용

--- js/script.js
+++ js/script.js
@@ -1,13 +1,37 @@
-document.addEventListener("DOMContentLoaded", () =>{
-    gsap.registerPlugin(ScrollTrigger);
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(ScrollTrigger);
 
-    // 헤더 메뉴 드롭다운
-    const header = document.querySelector("header");
-    const subBg = document.querySelector(".sub-bg");
-    const mainMenus = document.querySelectorAll(".main-menu>li");
+  // 헤더 메뉴 드롭다운
+  const header = document.querySelector("header");
+  const subBg = document.querySelector(".sub-bg");
+  const mainMenus = document.querySelectorAll(".main-menu>.gnb-menu");
+  const subMenu = document.querySelectorAll(".sub-menu");
 
-    // 서브메뉴 배경 높이
-    const subMenuHeight = 200;
-    
-    
-})
+  mainMenus.forEach((menu) => {
+    if (!subMenu) return;
+
+    menu.addEventListener("mouseenter", () => {
+      gsap.to(subBg, {
+        height: 150,
+        duration: 0.3,
+        ease: "power2.out",
+      });
+      gsap.to(subMenu, {
+        autoAlpha: 1,
+        y: 0,
+        duration: 0.3,
+        ease: "power2.out",
+      });
+    });
+
+    menu.addEventListener("mouseleave", () => {
+      gsap.to(subBg, {
+        height: 0,
+        duration: 0.3,
+        ease: "power2.out",
+      });
+    });
+  });
+});
+
+
